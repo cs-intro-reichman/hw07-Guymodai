74489ed982432bd700c421ec3afd4e4126d279ff
Guy Modai <Guy.modai@post.runi.ac.il>
2025-12-30 12:23:31 +0200

    Add IntToBin implementation

--- IntToBin.java
+++ IntToBin.java
@@ -7,8 +7,30 @@ public class IntToBin {
     }
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
-    public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
-    }    
- }
+    public static String toBinary(int x) 
+        {
+               String s = DecToBin1(x);
+               if (s.length() == 0)
+               {
+                   return "0";
+               }
+               else
+               {
+                   return s;
+               }
+        }
+    public static String DecToBin1(int number) {
+        if(number == 0)
+        {
+            return "";
+        }
+        if(number%2 ==0)
+        {
+           return DecToBin1(number/2) + "0";
+        }
+        else
+        {
+           return DecToBin1(number/2) + "1";
+        }
+  }
+}
