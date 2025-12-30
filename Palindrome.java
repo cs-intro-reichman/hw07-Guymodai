4af421be393c4e997bf8615bc0aef89007db7f48
Guy Modai <Guy.modai@post.runi.ac.il>
2025-12-30 13:30:23 +0200

    Add Palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -7,7 +7,14 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+		for(int i = 0 ; i < s.length()/2 ; i++)
+			{
+				if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(s.length() - 1 - i))) {
+					{
+						return false;
+					}
+			}
     }
+		return true;
+	}
 }
